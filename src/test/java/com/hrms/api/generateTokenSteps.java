16d6f98f4c6a6f2e3155d8f6dffa15b48edf72cb
ChristinaPROG <cwag.ramirez4@gmail.com>
2021-01-24 11:33:57 -0500

    3/6

--- src/test/java/com/hrms/api/generateTokenSteps.java
+++ src/test/java/com/hrms/api/generateTokenSteps.java
@@ -1,28 +1,27 @@
-package com.hrms.api;
+package com.hrms.API;
 
-import com.hrms.utils.apiConstants;
 import io.cucumber.java.en.Given;
+
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 
 import static io.restassured.RestAssured.given;
 
-public class generateTokenSteps {
+    public class generateTokenSteps {
+        String BaseURI= RestAssured.baseURI="http://3.237.189.167/syntaxapi/api";
 
-    static String token;
-    @Given("a JWT is generated")
+        @Given("a JWT is generated")
 
-    public void a_JWT_is_generated() {
+        public void a_JWT_is_generated() {
 
-        RequestSpecification generateTokenRequest = given().header("Content-type", "application/json")
-                .body("{\"email\":\"Moazzamsadiq85@gmail.com\"" +
-                        ",\"password\":\"123456789\"}");
-        Response generateTokenResponse = generateTokenRequest.when().post(apiConstants.GENERATE_TOKEN_URI);
-        generateTokenResponse.prettyPrint();
+            RequestSpecification generateTokenRequest = given().header("Content-type", "application/json")
+                    .body("{\"email\":\"Moazzamsadiq85@gmail.com\"" +
+                            ",\"password\":\"123456789\"}");
+            Response generateTokenResponse = generateTokenRequest.when().post("/generateToken.php");
+            generateTokenResponse.prettyPrint();
 
-        token="Bearer "+generateTokenResponse.jsonPath().getString("token");
+
+        }
 
     }
-
-}
--- src/test/java/com/hrms/stepdefinitions/FunctionTest.java
+++ src/test/java/com/hrms/stepdefinitions/FunctionTest.java
@@ -1,25 +1,22 @@
 package com.hrms.stepdefinitions;
 
-import java.util.List;
-import java.util.Map;
-
+import com.google.common.collect.Maps;
 import com.hrms.utils.ConfigsReader;
 import com.hrms.utils.Constants;
 import com.hrms.utils.DbUtils;
 
+import java.util.List;
+import java.util.Map;
+
 public class FunctionTest {
+    public static void main(String[] args) {
+        ConfigsReader.readProperties(Constants.CONFIGURATION_FILEPATH);
 
-	public static void main(String[] args) {
+        String query = "select emp_firstname, emp_lastname from hs_hr_employees";
 
-		ConfigsReader.readProperties(Constants.CONFIGURATION_FILEPATH);
+        List<Map<String, String>> list = DbUtils.getDbDataIntoList(query);
 
-		String query = "select emp_firstname, emp_lastname from hs_hr_employees";
-
-		List<Map<String, String>> list = DbUtils.getDbDataIntoList(query);
-
-		System.out.println(list);
-		System.out.println(list.size());
-
-	}
-
+        System.out.println(list);
+        System.out.println(list.size());
+    }
 }
